9afbc1bd4d58d5839ef0d12fe7d952ba6457316e
Carter Bohin <carterbohin@gmail.com>
2024-11-26 15:11:12 -0700

    fixed now on to css

--- data/hotels.js
+++ data/hotels.js
@@ -1,10 +1,10 @@
 export const hotels = [
-  {  "name": "The Sutton Place", "location": "Toronto, CANADA", "stars": 5, "phone": "416-915-3770", "address": "355 King St W, Toronto, Ontario ON M5V 1J5 Canada", "photo":"01-washington.jpg"},
-  {  "name": "Kimpton Hotel", "location": "Monaco, Pittsburghkimpton", "stars": 5, "phone": "412-471-1170", "address": "620 William Penn Place, Pittsburgh, Pennsylvania 15219 United States", "photo":"01-washington.jpg"},
-  {  "name": "The Colony Hotel", "location": "Palm Beach, Floridacolony", "stars": 5, "phone": "561-655-5430", "address": "155 Hammon Ave, Palm Beach, Florida 33480 USA", "photo":"01-washington.jpg"},
-  {  "name": "Four Seasons Hotel", "location": "Montrealfour, CANADA", "stars": 5, "phone": "514-843-2500", "address": "1440 Rue de la Montagne, Montreal, Quebec H3G 1Z5 Canada", "photo":"01-washington.jpg"},
-  {  "name": "The Ritz-Carlton", "location": "Georgetown, Washington D.C.r", "stars": 5, "phone": "416-915-3770", "address": "3100 S St NW, Washington, D.C. 20007 United States", "photo":"01-washington.jpg"},
-  {  "name": "The Grand America Hotel", "location": "Salt Lake City, Utahgrand", "stars": 5, "phone": "801-258-6000", "address": "555 Main St., Salt Lake City, Utah 84111 United States", "photo":"01-washington.jpg"},
-  {  "name": "S Hotel Jamaica", "location": "Montego Bayja", "stars": 5, "phone": "876-979-0000", "address": "7 Jimmy Cliff Ave., Montego Bay, Jamaica", "photo":"01-washington.jpg"},
-  {  "name": "Katikies Santorini", "location": "Toronto, CANADA", "stars": 5, "phone": "+30 2286-071401", "address": "Santorini Greece", "photo":"01-washington.jpg"},
+  {  "name": "The Sutton Place", "location": "Toronto, CANADA", "phone": "416-915-3770", "address": "355 King St W, Toronto, Ontario ON M5V 1J5 Canada","year": "2024", "photo":"images/sutton.webp"},
+  {  "name": "Kimpton Hotel", "location": "Monaco, Pittsburghkimpton", "phone": "412-471-1170", "address": "620 William Penn Place, Pittsburgh, Pennsylvania 15219 United States","year": "2024", "photo":"images/kimpton.webp"},
+  {  "name": "The Colony Hotel", "location": "Palm Beach, Floridacolony", "phone": "561-655-5430", "address": "155 Hammon Ave, Palm Beach, Florida 33480 USA","year": "2024", "photo":"images/colony.webp"},
+  {  "name": "Four Seasons Hotel", "location": "Montrealfour, CANADA", "phone": "514-843-2500", "address": "1440 Rue de la Montagne, Montreal, Quebec H3G 1Z5 Canada","year": "2024", "photo":"images/fourseasons.webp"},
+  {  "name": "The Ritz-Carlton", "location": "Georgetown, Washington D.C.r", "phone": "416-915-3770", "address": "3100 S St NW, Washington, D.C. 20007 United States","year": "2024", "photo":"images/ritz.webp"},
+  {  "name": "The Grand America Hotel", "location": "Salt Lake City, Utahgrand", "phone": "801-258-6000", "address": "555 Main St., Salt Lake City, Utah 84111 United States","year": "2024", "photo":"images/grand.webp"},
+  {  "name": "S Hotel Jamaica", "location": "Montego Bayja", "phone": "876-979-0000", "address": "7 Jimmy Cliff Ave., Montego Bay, Jamaica","year": "2024", "photo":"images/jamaica.webp"},
+  {  "name": "Katikies Santorini", "location": "Toronto, CANADA", "phone": "+30 2286-071401", "address": "Santorini Greece","year": "2024", "photo":"images/katikies.webp"},
 ]
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,37 @@
+import { hotels } from '../data/hotels.js';
+
 const hb = document.querySelector('#hamburgerBtn');
 const pw = document.querySelector('#pageWrapper');
 
 hb.addEventListener('click', () => {
     pw.classList.toggle('moveOver');
+});
+
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear();
+
+const cards = document.querySelector('#allHotels');
+
+hotels.forEach(hotel => {
+    const mySection = document.createElement('section');
+
+    const theImage = document.createElement('img');
+    theImage.src = hotel.photo;
+    theImage.alt = `${hotel.name} (${hotel.year})`;
+
+    const theName = document.createElement('h2');
+    theName.textContent = hotel.name;
+
+    const theAddress = document.createElement('p');
+    theAddress.textContent = hotel.address;
+
+    const thePhone = document.createElement('p');
+    thePhone.textContent = hotel.phone;
+
+    mySection.appendChild(theImage);
+    mySection.appendChild(theName);
+    mySection.appendChild(theAddress);
+    mySection.appendChild(thePhone);
+
+    cards.appendChild(mySection);
 });
