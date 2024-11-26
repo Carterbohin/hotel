9561b190be31be0bd29625d9380c024106adeb27
Carter Bohin <carterbohin@gmail.com>
2024-11-26 14:40:28 -0700

    lots of changes

---
+++ data/hotels.js
@@ -0,0 +1,10 @@
+export const hotels = [
+  {  "name": "The Sutton Place", "location": "Toronto, CANADA", "stars": 5, "phone": "416-915-3770", "address": "355 King St W, Toronto, Ontario ON M5V 1J5 Canada", "photo":"01-washington.jpg"},
+  {  "name": "Kimpton Hotel", "location": "Monaco, Pittsburghkimpton", "stars": 5, "phone": "412-471-1170", "address": "620 William Penn Place, Pittsburgh, Pennsylvania 15219 United States", "photo":"01-washington.jpg"},
+  {  "name": "The Colony Hotel", "location": "Palm Beach, Floridacolony", "stars": 5, "phone": "561-655-5430", "address": "155 Hammon Ave, Palm Beach, Florida 33480 USA", "photo":"01-washington.jpg"},
+  {  "name": "Four Seasons Hotel", "location": "Montrealfour, CANADA", "stars": 5, "phone": "514-843-2500", "address": "1440 Rue de la Montagne, Montreal, Quebec H3G 1Z5 Canada", "photo":"01-washington.jpg"},
+  {  "name": "The Ritz-Carlton", "location": "Georgetown, Washington D.C.r", "stars": 5, "phone": "416-915-3770", "address": "3100 S St NW, Washington, D.C. 20007 United States", "photo":"01-washington.jpg"},
+  {  "name": "The Grand America Hotel", "location": "Salt Lake City, Utahgrand", "stars": 5, "phone": "801-258-6000", "address": "555 Main St., Salt Lake City, Utah 84111 United States", "photo":"01-washington.jpg"},
+  {  "name": "S Hotel Jamaica", "location": "Montego Bayja", "stars": 5, "phone": "876-979-0000", "address": "7 Jimmy Cliff Ave., Montego Bay, Jamaica", "photo":"01-washington.jpg"},
+  {  "name": "Katikies Santorini", "location": "Toronto, CANADA", "stars": 5, "phone": "+30 2286-071401", "address": "Santorini Greece", "photo":"01-washington.jpg"},
+]
